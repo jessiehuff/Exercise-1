fc2bdc2b07575e671b88700165e9fcf763dd7f21
Jessie Huff <huffjd1@gmail.com>
2019-02-25 23:00:51 -0500

    Finished mocha unit test

--- Test/test.js
+++ Test/test.js
@@ -1,12 +1,15 @@
-const debugging = require('./Exercise 1/debugging');
+const debugging = require('../debugging.js');
 const assert = require('assert');
 
 describe("remoteMathService", function() {
-    let one = 1;
-    let two = 2 ; 
-    let expected = one + two; 
     
     it('correctly calculates the sum of 1 and 2', () => {
-        assert.equal(debugging.remoteMathService(), expected); 
-    });  //having trouble calling the function with all the callbacks 
+        assert.equal(debugging.remoteMathService(function (err, answer) {
+            var noError = (err===undefined) 
+            var correctAnswer = (3 === answer)
+        
+            return noError && correctAnswer 
+        }), true); 
+    });   
 })
+
